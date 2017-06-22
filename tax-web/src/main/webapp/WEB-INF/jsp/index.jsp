14c7f19476776ffbd0779575ccba35a02c210082
曾小燕 <13510460187@163.com>
2017-06-22 18:35:07 +0800

    添加了测试action代码。待测试

---
+++ tax-web/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>测试整合！</title>
+</head>
+<body>
+	<h1>struts整合成功</h1>
+	<div>${responseData}</div>
+</body>
+</html>
---
+++ tax-web/src/test/java/cn/tax/action/TestAction.java
@@ -0,0 +1,19 @@
+package cn.tax.action;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.tax.service.TestService;
+
+public class TestAction extends ActionSupport  {
+	@Autowired
+	private TestService testService;
+	
+	@Override
+	public String execute() throws Exception {
+		ActionContext.getContext().put("responseData", "您好");
+		testService.sayHello();
+		return SUCCESS;
+	}
+}
