bc3fe0418a3bfd43c0a103f7843367a5a3d08a4a
曾小燕 <13510460187@163.com>
2017-06-28 16:35:26 +0800

    添加了日志

--- tax-web/src/main/java/com/test/action/TestSpring.java
+++ tax-web/src/main/java/com/test/action/TestSpring.java
@@ -1,5 +1,6 @@
 package com.test.action;
 
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,6 +10,7 @@ import com.test.service.TestHibernate;
 
 public class TestSpring {
 	private ClassPathXmlApplicationContext context;
+	
 	@Before
 	public void setUp() throws Exception {
 		context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
@@ -20,6 +22,15 @@ public class TestSpring {
 		Student s = new Student();
 		s.setName("小沈阳");
 		testHibernate.saveStudent(s);
+	}
+	
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(TestSpring.class);
+		logger.debug("这是debug");
+		logger.info("这是Info");
+		logger.error("这是error");
+		int i = 1/0;
+		
 	}
 
 }
