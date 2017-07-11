3d5ebbab1860e17cb0dd7feaf2d2168676dac1a5
zxy1994 <13510460187@163.com>
2017-07-11 10:07:03 +0800

    开始开发

---
+++ tax-web/src/main/java/com/tax/web/user/UserAction.java
@@ -0,0 +1,21 @@
+package com.tax.web.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.tax.service.UserService;
+
+/**
+ * UserAction
+ * @author   ZENG.XIAO.YAN
+ * @date 	 2017年7月11日 上午10:06:05
+ * @version  v1.0
+ */
+
+public class UserAction extends ActionSupport {
+
+	private static final long serialVersionUID = 4526496105243102063L;
+	@Autowired
+	private UserService userService;
+
+}
