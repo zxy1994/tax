ae06e68b0db1556cbb92f33a443806851ed3eb21
zxy1994 <13510460187@163.com>
2017-07-15 16:49:17 +0800

    20170715

--- tax-service/src/main/java/com/tax/service/UserService.java
+++ tax-service/src/main/java/com/tax/service/UserService.java
@@ -1,7 +1,42 @@
 package com.tax.service;
 
+import java.io.Serializable;
+import java.util.List;
+
 import com.tax.pojo.nsfw.User;
 
 public interface UserService {
-	void saveUser(User user);
+	
+	/**
+	 * 新增
+	 * @param user
+	 */
+	public void save(User user);
+
+	/**
+	 * 更新
+	 * @param user
+	 */
+	public void update(User user);
+
+	/**
+	 * 根据id删除
+	 * @param id
+	 */
+	public void deleteById(Serializable id);
+
+	
+	/**
+	 * 通过id查找
+	 * @param id
+	 * @return 实体
+	 */
+	public User findById(Serializable id);
+
+	
+	/**
+	 * 查找所有
+	 * @return List集合
+	 */
+	public List<User> findAll();
 }
--- tax-service/src/main/java/com/tax/service/impl/UserServiceImpl.java
+++ tax-service/src/main/java/com/tax/service/impl/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.tax.service.impl;
 
+import java.io.Serializable;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +16,29 @@ public class UserServiceImpl implements UserService {
 	private UserDao userDao;
 
 	@Override
-	public void saveUser(User user) {
+	public void save(User user) {
 		userDao.save(user);
 	}
+
+	@Override
+	public void update(User user) {
+		userDao.update(user);
+	}
+
+	@Override
+	public void deleteById(Serializable id) {
+		userDao.deleteById(id);
+	}
+
+	@Override
+	public User findById(Serializable id) {
+		return userDao.findById(id);
+	}
+
+	@Override
+	public List<User> findAll() {
+		return userDao.findAll();
+	}
+
+	
 }
