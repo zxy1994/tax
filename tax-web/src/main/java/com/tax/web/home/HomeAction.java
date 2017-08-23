ec654158a0c1b903f0df50fa6e054f76f89e8d14
zxy1994 <13510460187@163.com>
2017-08-23 18:33:10 +0800

    u

---
+++ tax-web/src/main/java/com/tax/web/home/HomeAction.java
@@ -0,0 +1,16 @@
+package com.tax.web.home;
+
+import java.io.Serializable;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * HomeAction
+ * @author   ZENG.XIAO.YAN
+ * @date 	 2017年8月23日 下午5:25:28
+ * @version  v1.0
+ */
+
+public class HomeAction extends ActionSupport implements Serializable {
+
+}
