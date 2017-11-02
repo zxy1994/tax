f06fe66be70262e15dc687be38b7b5c75d1ad449
zxy1994 <13510460187@163.com>
2017-11-02 23:46:27 +0800

    baseService

---
+++ tax-common/src/main/java/com/tax/core/service/BaseService.java
@@ -0,0 +1,46 @@
+package com.tax.core.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+
+/**
+ * BaseService
+ * @author 	ZENG.XIAO.YAN
+ * @date 	2017年10月29日 下午4:53:53
+ * @version v1.0
+ */
+public interface BaseService<T> {
+	/**
+	 * 新增
+	 * @param entity
+	 */
+	public void save(T entity);
+
+	/**
+	 * 更新
+	 * @param entity
+	 */
+	public void update(T entity);
+
+	/**
+	 * 根据id删除
+	 * @param id
+	 */
+	public void deleteById(Serializable id);
+
+	
+	/**
+	 * 通过id查找
+	 * @param id
+	 * @return 实体
+	 */
+	public T findById(Serializable id);
+
+	
+	/**
+	 * 查找所有
+	 * @return List集合
+	 */
+	public List<T> findAll();
+}
---
+++ tax-common/src/main/java/com/tax/core/service/impl/BaseServiceImpl.java
@@ -0,0 +1,47 @@
+package com.tax.core.service.impl;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.tax.core.dao.BaseDao;
+import com.tax.core.service.BaseService;
+
+/**
+ * BaseServiceImpl
+ * @author 	ZENG.XIAO.YAN
+ * @date 	2017年10月29日 下午4:53:48
+ * @version v1.0
+ */
+public class BaseServiceImpl<T> implements BaseService<T> {
+	private BaseDao<T> baseDao; // 后续接收子类dao
+	@Override
+	public void save(T entity) {
+		baseDao.save(entity);
+	}
+
+	@Override
+	public void update(T entity) {
+		baseDao.update(entity);
+	}
+
+	@Override
+	public void deleteById(Serializable id) {
+		baseDao.deleteById(id);
+	}
+
+	@Override
+	public T findById(Serializable id) {
+		return baseDao.findById(id);
+	}
+
+	@Override
+	public List<T> findAll() {
+		return baseDao.findAll();
+	}
+
+	public void setBaseDao(BaseDao<T> baseDao) {
+		this.baseDao = baseDao;
+	}
+	
+
+}
