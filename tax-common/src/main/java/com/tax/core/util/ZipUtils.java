056d0755b81a1043c7a78f1d4e14d03cbbe3174a
zxy1994 <13510460187@163.com>
2017-11-19 22:08:51 +0800

    完成了ftl到excel表格的转换，同时完成了下载zip压缩包

---
+++ tax-common/src/main/java/com/tax/core/util/ZipUtils.java
@@ -0,0 +1,92 @@
+package com.tax.core.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * ZipUtils
+ * @author 	ZENG.XIAO.YAN
+ * @date 	2017年11月19日 下午7:16:08
+ * @version v1.0
+ */
+public class ZipUtils {
+	
+	/**
+	 * 压缩成zip
+	 * @param srcDir 压缩文件夹路径 
+	 * @param out    压缩文件输出流
+	 * @throws RuntimeException  压缩失败会抛出运行时异常
+	 */
+	public static void toZip(String srcDir, OutputStream out)throws RuntimeException{
+		System.out.println("压缩中....");
+		long start = System.currentTimeMillis();
+		ZipOutputStream zos = null ;
+		try {
+			zos = new ZipOutputStream(out);
+			File sourceFile = new File(srcDir);
+			compress(sourceFile,zos,sourceFile.getName());
+			zos.close();
+			System.out.println("压缩..完成");
+			long end = System.currentTimeMillis();
+			System.out.println("压缩耗时：" + (end - start) +" ms");
+		} catch (Exception e) {
+			throw new RuntimeException("zip error from ZipUtils",e);
+		}finally{
+			if(zos != null){
+				try {
+					zos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+
+	/**
+	 * 递归压缩方法
+	 * @param sourceFile 源文件
+	 * @param zos		 zip输出流
+	 * @param name		 压缩后的名称
+	 * @throws Exception
+	 */
+	private static void compress(File sourceFile, ZipOutputStream zos, String name) throws Exception{
+		byte[] buf = new byte[1024];
+		if(sourceFile.isFile()){
+			// 向zip输出流中添加一个zip实体，构造器中name为zip实体的文件的名字
+			zos.putNextEntry(new ZipEntry(name));
+			// copy文件到zip输出流中
+			int len;
+			FileInputStream in = new FileInputStream(sourceFile);
+			while ((len = in.read(buf)) > 0){
+				zos.write(buf, 0, len);
+			}
+			// Complete the entry
+			zos.closeEntry();
+			in.close();
+		} else {
+			File[] listFiles = sourceFile.listFiles();
+			if(listFiles == null || listFiles.length == 0){
+				// 空文件夹的处理
+				zos.putNextEntry(new ZipEntry(name+"/"));
+				// 没有文件，不需要文件的copy
+				zos.closeEntry();
+			}else {
+				for (File file : listFiles) {
+					// 注意：name需要带上父文件夹的名字，不然最后压缩包中就没有文件夹了
+					compress(file, zos, name + "/" + file.getName());
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		FileOutputStream fos = new FileOutputStream(new File("c:/mytest.zip"));
+		ZipUtils.toZip("D:/log", fos);
+	}
+}
---
+++ tax-web/src/main/java/com/tax/web/nsfw/FTL2ExcelByFreeMarker.java
@@ -0,0 +1,88 @@
+package com.tax.web.nsfw;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.tax.core.util.ZipUtils;
+import com.tax.pojo.nsfw.User;
+import com.tax.service.nsfw.UserService;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+/**
+ * FTL2ExcelByFreeMarker
+ * 导出Excel通过FreeMarker测试类
+ * @author 	ZENG.XIAO.YAN
+ * @date 	2017年11月17日 下午10:12:15
+ * @version v1.0
+ */
+public class FTL2ExcelByFreeMarker extends ActionSupport {
+
+	/** serialVersionUID */
+	private static final long serialVersionUID = 1L;
+	@Autowired
+	private UserService userService;
+	
+	@SuppressWarnings("resource")
+	public void export(){
+		try {
+			// 从数据库查出数据
+			List<User> uList = userService.findAll();
+			// 封装数据
+			Map<String, Object> dataMap = new HashMap<String,Object>();
+			List<Map<String,String>> userList = new ArrayList<>(uList.size());
+			for(User u : uList) {
+				Map<String,String> rowData = new HashMap<>();
+				rowData.put("name", u.getName());
+				rowData.put("account", u.getAccount());
+				rowData.put("dept", u.getDept());
+				rowData.put("gender", u.isGender() ? "男":"女");
+				rowData.put("email", u.getEmail());
+				userList.add(rowData);
+			}
+			if(userList.size() > 0){
+				HttpServletRequest request = ServletActionContext.getRequest();
+				FileWriter writer;
+				for (int i = 0; i < 10; i++) {
+					dataMap.put("userList", userList);
+					Map<String, String> endMap = new HashMap<>();
+					endMap.put("user", "老王");
+					endMap.put("time", "2017-10-10 10:50:55");
+					dataMap.put("endMap", endMap);
+					Configuration cfg = new Configuration();
+					cfg.setServletContextForTemplateLoading(ServletActionContext.getServletContext(), "/ftl");
+					Template template = cfg.getTemplate("exportExcel.ftl");
+					String rootPath = request.getRealPath("/");
+					File temDir = new File(rootPath + "/download");
+					if(!temDir.exists()){
+						temDir.mkdirs();
+					}
+					writer = new FileWriter(temDir.getPath()+"/excel"+ i +".xls");
+					template.process(dataMap, writer);
+					writer.flush();
+					writer.close();
+				}
+				HttpServletResponse response = ServletActionContext.getResponse();
+				response.setContentType("application/zip");
+				response.setHeader("Content-Disposition", "attachment; filename=excel.zip");  
+				ZipUtils.toZip(request.getRealPath("/")+"/download", response.getOutputStream());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//return NONE;
+	}
+}
