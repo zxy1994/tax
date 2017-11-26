138ea205fc06d2feb224fb3d9bcaed428de80696
zxy1994 <13510460187@163.com>
2017-11-26 15:27:14 +0800

    u

--- tax-web/src/main/java/com/tax/web/nsfw/FTL2ExcelByFreeMarker.java
+++ tax-web/src/main/java/com/tax/web/nsfw/FTL2ExcelByFreeMarker.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -53,31 +54,37 @@ public class FTL2ExcelByFreeMarker extends ActionSupport {
 				userList.add(rowData);
 			}
 			if(userList.size() > 0){
+				/** 下面为下载zip压缩包相关流程 */
 				HttpServletRequest request = ServletActionContext.getRequest();
 				FileWriter writer;
+				/** 1.创建临时文件夹  */
+				String rootPath = request.getSession().getServletContext().getRealPath("/");
+				File temDir = new File(rootPath + "/" + UUID.randomUUID().toString().replaceAll("_", ""));
+				if(!temDir.exists()){
+					temDir.mkdirs();
+				}
+				/** 2.生成需要下载的文件，存放在临时文件夹内 */
 				for (int i = 0; i < 10; i++) {
 					dataMap.put("userList", userList);
 					Map<String, String> endMap = new HashMap<>();
 					endMap.put("user", "老王");
 					endMap.put("time", "2017-10-10 10:50:55");
 					dataMap.put("endMap", endMap);
-					Configuration cfg = new Configuration();
+					Configuration cfg = new Configuration(Configuration.VERSION_2_3_22);
 					cfg.setServletContextForTemplateLoading(ServletActionContext.getServletContext(), "/ftl");
 					Template template = cfg.getTemplate("exportExcel.ftl");
-					String rootPath = request.getRealPath("/");
-					File temDir = new File(rootPath + "/download");
-					if(!temDir.exists()){
-						temDir.mkdirs();
-					}
 					writer = new FileWriter(temDir.getPath()+"/excel"+ i +".xls");
 					template.process(dataMap, writer);
 					writer.flush();
 					writer.close();
 				}
+				/** 3.设置response的header */
 				HttpServletResponse response = ServletActionContext.getResponse();
 				response.setContentType("application/zip");
 				response.setHeader("Content-Disposition", "attachment; filename=excel.zip");  
-				ZipUtils.toZip(request.getRealPath("/")+"/download", response.getOutputStream());
+				System.out.println(temDir.getPath());
+				/** 4.调用工具类，下载zip压缩包 */
+				ZipUtils.toZip(temDir.getPath(), response.getOutputStream());
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
