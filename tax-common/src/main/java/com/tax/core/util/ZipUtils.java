ca02c47abf37c30beb3922918cc2c11ae1a2615e
zxy1994 <13510460187@163.com>
2017-12-05 09:57:33 +0800

    优化zipUtils

--- tax-common/src/main/java/com/tax/core/util/ZipUtils.java
+++ tax-common/src/main/java/com/tax/core/util/ZipUtils.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -19,27 +21,24 @@ public class ZipUtils {
 	private static final int  BUFFER_SIZE = 2 * 1024;
 	
 	/**
-	 * 压缩成zip
+	 * 压缩成ZIP 方法1
 	 * @param srcDir 压缩文件夹路径 
 	 * @param out    压缩文件输出流
 	 * @param KeepDirStructure  是否保留原来的目录结构,true:保留目录结构; 
 	 * 							false:所有文件跑到压缩包根目录下(注意：不保留目录结构可能会出现同名文件,会压缩失败)
 	 * @throws RuntimeException 压缩失败会抛出运行时异常
 	 */
-	
-	public static void toZip(String srcDir, OutputStream out, 
-			boolean KeepDirStructure)throws RuntimeException{
-		System.out.println("压缩中....");
+	public static void toZip(String srcDir, OutputStream out, boolean KeepDirStructure)
+			throws RuntimeException{
+		
 		long start = System.currentTimeMillis();
 		ZipOutputStream zos = null ;
 		try {
 			zos = new ZipOutputStream(out);
 			File sourceFile = new File(srcDir);
 			compress(sourceFile,zos,sourceFile.getName(),KeepDirStructure);
-			zos.close();
-			System.out.println("压缩..完成");
 			long end = System.currentTimeMillis();
-			System.out.println("压缩耗时：" + (end - start) +" ms");
+			System.out.println("压缩完成，耗时：" + (end - start) +" ms");
 		} catch (Exception e) {
 			throw new RuntimeException("zip error from ZipUtils",e);
 		}finally{
@@ -53,7 +52,46 @@ public class ZipUtils {
 		}
 		
 	}
-
+	
+	
+	/**
+	 * 压缩成ZIP 方法2
+	 * @param srcFiles 需要压缩的文件列表
+	 * @param out 	        压缩文件输出流
+	 * @throws RuntimeException 压缩失败会抛出运行时异常
+	 */
+	public static void toZip(List<File> srcFiles , OutputStream out)throws RuntimeException {
+		long start = System.currentTimeMillis();
+		ZipOutputStream zos = null ;
+		try {
+			zos = new ZipOutputStream(out);
+			for (File srcFile : srcFiles) {
+				byte[] buf = new byte[BUFFER_SIZE];
+				zos.putNextEntry(new ZipEntry(srcFile.getName()));
+				int len;
+				FileInputStream in = new FileInputStream(srcFile);
+				while ((len = in.read(buf)) != -1){
+					zos.write(buf, 0, len);
+				}
+				zos.closeEntry();
+				in.close();
+			}
+			long end = System.currentTimeMillis();
+			System.out.println("压缩完成，耗时：" + (end - start) +" ms");
+		} catch (Exception e) {
+			throw new RuntimeException("zip error from ZipUtils",e);
+		}finally{
+			if(zos != null){
+				try {
+					zos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	
 	/**
 	 * 递归压缩方法
 	 * @param sourceFile 源文件
@@ -106,7 +144,15 @@ public class ZipUtils {
 	}
 	
 	public static void main(String[] args) throws Exception {
-		FileOutputStream fos = new FileOutputStream(new File("c:/mytest.zip"));
-		ZipUtils.toZip("D:/log", fos,true);
+		/** 测试压缩方法1  */
+		FileOutputStream fos1 = new FileOutputStream(new File("c:/mytest01.zip"));
+		ZipUtils.toZip("D:/log", fos1,true);
+		
+		/** 测试压缩方法2  */
+		List<File> fileList = new ArrayList<>();
+		fileList.add(new File("D:/Java/jdk1.7.0_45_64bit/bin/jar.exe"));
+		fileList.add(new File("D:/Java/jdk1.7.0_45_64bit/bin/java.exe"));
+		FileOutputStream fos2 = new FileOutputStream(new File("c:/mytest02.zip"));
+		ZipUtils.toZip(fileList, fos2);
 	}
 }
