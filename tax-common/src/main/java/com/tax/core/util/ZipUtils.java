39f581143e5532e22dfe370313c08ad62e97dd93
zxy1994 <13510460187@163.com>
2017-12-25 22:30:51 +0800

    增加了zip解压的方法

--- tax-common/src/main/java/com/tax/core/util/ZipUtils.java
+++ tax-common/src/main/java/com/tax/core/util/ZipUtils.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
 /**
@@ -143,6 +146,72 @@ public class ZipUtils {
 		}
 	}
 	
+	/****************************解压相关代码************************************/
+	
+	/**
+	 * zip解压
+	 * @param srcFile        zip源文件
+	 * @param destDirPath	  解压后的目标文件夹
+	 * @throws RuntimeException 解压失败会抛出运行时异常
+	 */
+	public static void unZip(File srcFile, String destDirPath) throws RuntimeException {
+		long start = System.currentTimeMillis();
+		// 判断源文件是否存在
+		if (!srcFile.exists()) {
+			throw new RuntimeException(srcFile.getPath() + "所指文件不存在");
+		}
+		// 开始解压
+		ZipFile zipFile = null;
+		try {
+			zipFile = new ZipFile(srcFile);
+			Enumeration<?> entries = zipFile.entries();
+			while (entries.hasMoreElements()) {
+				ZipEntry entry = (ZipEntry) entries.nextElement();
+				System.out.println("解压" + entry.getName());
+				// 如果是文件夹，就创建个文件夹
+				if (entry.isDirectory()) {
+					String dirPath = destDirPath + "/" + entry.getName();
+					File dir = new File(dirPath);
+					dir.mkdirs();
+				} else {
+					// 如果是文件，就先创建一个文件，然后用io流把内容copy过去
+					File targetFile = new File(destDirPath + "/" + entry.getName());
+					// 保证这个文件的父文件夹必须要存在
+					if(!targetFile.getParentFile().exists()){
+						targetFile.getParentFile().mkdirs();
+					}
+					targetFile.createNewFile();
+					// 将压缩文件内容写入到这个文件中
+					InputStream is = zipFile.getInputStream(entry);
+					FileOutputStream fos = new FileOutputStream(targetFile);
+					int len;
+					byte[] buf = new byte[BUFFER_SIZE];
+					while ((len = is.read(buf)) != -1) {
+						fos.write(buf, 0, len);
+					}
+					is.close();
+					fos.close();
+				}
+			}
+			long end = System.currentTimeMillis();
+			System.out.println("解压完成，耗时：" + (end - start) +" ms");
+		} catch (Exception e) {
+			throw new RuntimeException("unzip error from ZipUtils", e);
+		} finally {
+			if(zipFile != null){
+				try {
+					zipFile.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	
+	
+	
+	
 	public static void main(String[] args) throws Exception {
 		/** 测试压缩方法1  */
 		FileOutputStream fos1 = new FileOutputStream(new File("c:/mytest01.zip"));
@@ -154,5 +223,9 @@ public class ZipUtils {
 		fileList.add(new File("D:/Java/jdk1.7.0_45_64bit/bin/java.exe"));
 		FileOutputStream fos2 = new FileOutputStream(new File("c:/mytest02.zip"));
 		ZipUtils.toZip(fileList, fos2);
+		
+		/** 测试解压缩方法  */
+		File srcFile = new File("D:/ziptmp/udacs-hw-proxy-test.zip");
+		ZipUtils.unZip(srcFile, "c:/unzip");
 	}
 }
